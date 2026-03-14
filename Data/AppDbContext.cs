a028af8377d42da011d2336e0efc6d87850ec78c
mihor <mihor@pm.me>
2026-03-14 21:15:16 +0100

    Rework migrations and fix context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,28 +14,28 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // tenant-a
-        User alice = new User { Id = new Guid("bef81bfc-2cbb-4321-bd4a-cecb244dadcb"), Username = "alice", Email = "alice@tenant-a.example.com" };
-        User bob = new User { Id = new Guid("657ca4fa-fb2d-4180-80db-1403c6b8579e"), Username = "bob", Email = "bob@tenant-a.example.com" };
+        User alice = new User { Id = Guid.Parse("bef81bfc-2cbb-4321-bd4a-cecb244dadcb"), Username = "alice", Email = "alice@tenant-a.example.com" };
+        User bob = new User { Id = Guid.Parse("657ca4fa-fb2d-4180-80db-1403c6b8579e"), Username = "bob", Email = "bob@tenant-a.example.com" };
 
         // tenant-b
-        User carol = new User { Id = new Guid("1b33930d-4437-41ee-9b10-a864b40cec78"), Username = "carol", Email = "carol@tenant-b.example.com" };
-        User dan = new User { Id = new Guid("420f5b2e-ef55-4733-8bd9-053a07b9ed9c"), Username = "dan", Email = "dan@tenant-b.example.com" };
+        User carol = new User { Id = Guid.Parse("1b33930d-4437-41ee-9b10-a864b40cec78"), Username = "carol", Email = "carol@tenant-b.example.com" };
+        User dan = new User { Id = Guid.Parse("420f5b2e-ef55-4733-8bd9-053a07b9ed9c"), Username = "dan", Email = "dan@tenant-b.example.com" };
 
         // tenant-c
-        User eve = new User { Id = new Guid("eaa46a1d-7ace-4ba8-9b2e-a4e4f8d654d1"), Username = "eve", Email = "eve@tenant-c.example.com" };
-        User frank = new User { Id = new Guid("13bac98c-abd2-424b-bf2f-e1c26dee0e71"), Username = "frank", Email = "frank@tenant-c.example.com" };
+        User eve = new User { Id = Guid.Parse("eaa46a1d-7ace-4ba8-9b2e-a4e4f8d654d1"), Username = "eve", Email = "eve@tenant-c.example.com" };
+        User frank = new User { Id = Guid.Parse("13bac98c-abd2-424b-bf2f-e1c26dee0e71"), Username = "frank", Email = "frank@tenant-c.example.com" };
 
         // no tenant
-        User grace = new User { Id = new Guid("e8f46251-0c7a-47c2-99b3-88c9496d8e9d"), Username = "grace", Email = "grace@no-tenant.example.com" };
+        User grace = new User { Id = Guid.Parse("e8f46251-0c7a-47c2-99b3-88c9496d8e9d"), Username = "grace", Email = "grace@no-tenant.example.com" };
 
 
         modelBuilder.Entity<User>().HasData(
          alice, bob, carol, dan, eve, frank, grace
         );
 
-        Tenant tenantA = new Tenant { Id = new Guid("4da30340-fda0-49b0-b564-f511c630d221"), Title = "Tenant-A", Enabled = true };
-        Tenant tenantB = new Tenant { Id = new Guid("2337e27f-58eb-4973-9b43-4b795dac1ad7"), Title = "Tenant-B", Enabled = true };
-        Tenant tenantC = new Tenant { Id = new Guid("4da16ab8-3f6b-4af6-9fba-82daa779aeb9"), Title = "Tenant-C", Enabled = true };
+        Tenant tenantA = new Tenant { Id = Guid.Parse("4da30340-fda0-49b0-b564-f511c630d221"), Title = "Tenant-A", Enabled = true };
+        Tenant tenantB = new Tenant { Id = Guid.Parse("2337e27f-58eb-4973-9b43-4b795dac1ad7"), Title = "Tenant-B", Enabled = true };
+        Tenant tenantC = new Tenant { Id = Guid.Parse("4da16ab8-3f6b-4af6-9fba-82daa779aeb9"), Title = "Tenant-C", Enabled = true };
 
         modelBuilder.Entity<Tenant>().HasData(
             tenantA, tenantB, tenantC
@@ -43,20 +43,21 @@ public class AppDbContext : DbContext
 
         TaskItem task1 = new TaskItem
         {
-            Id = new Guid(),
+            Id = Guid.Parse("d907410e-5860-4cc4-8800-2230895c001f"),
             Title = "Learn ASP.NET Core",
             Status = TaskStatus.New,
             AssignedUserId = carol.Id,
-            TenantId = tenantB.Id,
+            TenantId = carol.TenantId,
             DueDate = null
         };
+
         TaskItem task2 = new TaskItem
         {
-            Id = new Guid(),
+            Id = Guid.Parse("c534787f-dfb8-4269-8941-791efcb8c4e4"),
             Title = "Build Web API",
             Status = TaskStatus.New,
             AssignedUserId = alice.Id,
-            TenantId = tenantA.Id,
+            TenantId = alice.TenantId,
             DueDate = null
         };
 
@@ -64,12 +65,9 @@ public class AppDbContext : DbContext
             task1, task2
         );
 
-
         modelBuilder.Entity<TaskItem>()
           .HasOne(t => t.AssignedUser)
-          .WithMany(u => u.Tasks)
-          .HasForeignKey(t => t.AssignedUserId)
-          .HasForeignKey(t => t.TenantId);
+          .WithMany(u => u.Tasks);
 
         modelBuilder.Entity<TaskItem>(entity =>
         {
@@ -80,7 +78,10 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(t => t.Id);
-            entity.HasIndex(t => t.TenantId).IsUnique();
+            entity.HasIndex(t => t.Id).IsUnique();
         });
+
+        modelBuilder.Entity<UserTenant>()
+          .HasKey(x => new { x.UserId, x.TenantId });
     }
 }
---
+++ Migrations/20260314191133_InitialCreate.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Enabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    AssignedUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DueDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_AssignedUserId",
+                        column: x => x.AssignedUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTenant",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTenant", x => new { x.UserId, x.TenantId });
+                    table.ForeignKey(
+                        name: "FK_UserTenant_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTenant_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tenants",
+                columns: new[] { "Id", "Enabled", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("2337e27f-58eb-4973-9b43-4b795dac1ad7"), true, "Tenant-B" },
+                    { new Guid("4da16ab8-3f6b-4af6-9fba-82daa779aeb9"), true, "Tenant-C" },
+                    { new Guid("4da30340-fda0-49b0-b564-f511c630d221"), true, "Tenant-A" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "TenantId", "Username" },
+                values: new object[,]
+                {
+                    { new Guid("13bac98c-abd2-424b-bf2f-e1c26dee0e71"), "frank@tenant-c.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "frank" },
+                    { new Guid("1b33930d-4437-41ee-9b10-a864b40cec78"), "carol@tenant-b.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "carol" },
+                    { new Guid("420f5b2e-ef55-4733-8bd9-053a07b9ed9c"), "dan@tenant-b.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "dan" },
+                    { new Guid("657ca4fa-fb2d-4180-80db-1403c6b8579e"), "bob@tenant-a.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "bob" },
+                    { new Guid("bef81bfc-2cbb-4321-bd4a-cecb244dadcb"), "alice@tenant-a.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "alice" },
+                    { new Guid("e8f46251-0c7a-47c2-99b3-88c9496d8e9d"), "grace@no-tenant.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "grace" },
+                    { new Guid("eaa46a1d-7ace-4ba8-9b2e-a4e4f8d654d1"), "eve@tenant-c.example.com", new Guid("00000000-0000-0000-0000-000000000000"), "eve" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "AssignedUserId", "DueDate", "Status", "TenantId", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("c534787f-dfb8-4269-8941-791efcb8c4e4"), new Guid("bef81bfc-2cbb-4321-bd4a-cecb244dadcb"), null, 0, new Guid("00000000-0000-0000-0000-000000000000"), "Build Web API" },
+                    { new Guid("d907410e-5860-4cc4-8800-2230895c001f"), new Guid("1b33930d-4437-41ee-9b10-a864b40cec78"), null, 0, new Guid("00000000-0000-0000-0000-000000000000"), "Learn ASP.NET Core" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_AssignedUserId",
+                table: "Tasks",
+                column: "AssignedUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_TenantId",
+                table: "Tasks",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id",
+                table: "Users",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTenant_TenantId",
+                table: "UserTenant",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "UserTenant");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

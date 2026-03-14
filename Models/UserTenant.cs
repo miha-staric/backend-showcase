c76651e4f388531fae1b24b4f48dd01395341ae5
mihor <mihor@pm.me>
2026-03-14 21:15:28 +0100

    Add UserTenant model

---
+++ Models/UserTenant.cs
@@ -0,0 +1,8 @@
+public class UserTenant
+{
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+
+    public Guid TenantId { get; set; }
+    public Tenant Tenant { get; set; }
+}

59805c97edb2aafcf59d6532bdc978934829d42c
mihor <mihor@pm.me>
2026-03-17 14:50:07 +0100

    Fix task creation and add cache invalidation

--- Application/Tasks/Commands/CreateTaskCommandHandler.cs
+++ Application/Tasks/Commands/CreateTaskCommandHandler.cs
@@ -2,6 +2,7 @@ using Contracts;
 using MassTransit;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using ZiggyCreatures.Caching.Fusion;
 
 public class CreateTaskCommandHandler
     : IRequestHandler<CreateTaskCommand, TaskDto>
@@ -9,15 +10,18 @@ public class CreateTaskCommandHandler
     private readonly AppDbContext _db;
     private readonly IPublishEndpoint _publishEndpoint;
     private readonly ITenantContext _tenantContext;
+    private readonly IFusionCache _cache;
 
     public CreateTaskCommandHandler(
         AppDbContext db,
         IPublishEndpoint publishEndpoint,
-        ITenantContext tenantContext)
+        ITenantContext tenantContext,
+        IFusionCache cache)
     {
         _db = db;
         _publishEndpoint = publishEndpoint;
         _tenantContext = tenantContext;
+        _cache = cache;
     }
 
     public async Task<TaskDto> Handle(
@@ -25,6 +29,7 @@ public class CreateTaskCommandHandler
         CancellationToken cancellationToken)
     {
         Guid? tenantId = _tenantContext.TenantId;
+        string cacheKey = $"tenant:{tenantId}";
 
         var task = new TaskItem
         {
@@ -36,7 +41,33 @@ public class CreateTaskCommandHandler
             Status = TaskStatus.New
         };
 
+        var userId = request.AssignedUserId;
+
+        // Null or Guid.Empty is OK
+        if (userId == null || userId == Guid.Empty)
+        {
+            task.AssignedUserId = null;
+        }
+        else
+        {
+            Boolean userExists = await _db.Users
+                .AnyAsync(u => u.Id == userId);
+
+            if (!userExists)
+                throw new Exception("Assigned user does not exist.");
+
+            Boolean userTenantExists = await _db.UserTenant
+                .AnyAsync(ut => ut.UserId == userId && ut.TenantId == tenantId);
+
+            if (!userTenantExists)
+                throw new Exception("User is not part of this tenant");
+
+            task.AssignedUserId = userId;
+        }
+
         _db.Tasks.Add(task);
+
+        await _cache.RemoveAsync(cacheKey);
 
         await _db.SaveChangesAsync(cancellationToken);
 

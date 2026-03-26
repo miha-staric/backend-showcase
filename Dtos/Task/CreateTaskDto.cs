c4522d7894e03c804fe5e0c290f2cbdaaacf9e93
mihor <mihor@pm.me>
2026-03-26 14:22:06 +0100

    Remove unused Dtos

--- Dtos/Task/CreateTaskDto.cs
+++
@@ -1,8 +0,0 @@
-// used for POST
-public class CreateTaskDto
-{
-    public String Title { get; set; } = null!;
-    public String Status { get; set; } = "New";
-    public Guid AssignedUserId { get; set; }
-    public DateTime? DueDate { get; set; }
-}
